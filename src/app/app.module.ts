87992498965334b9d34dd1edef49f572bfd67e50
dayanandraut <dayanandraut2015@.com>
2019-05-31 11:33:50 +0530

    in-memory-db set up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// for in memory db
+import { InMemoryWebApiModule } from "angular-in-memory-web-api";  
+import { DataService } from "./data.service";
+// --------------------------------------------------------
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,7 +14,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    InMemoryWebApiModule.forRoot(DataService) // for in memory db
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/data.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import {InMemoryDbService} from 'angular-in-memory-web-api';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService implements InMemoryDbService {
+
+  constructor() { }
+
+  createDb(){
+    let data=[
+      {id:1, title:"Game of Thrones", favouriteCharacter:"Tyrion", recentlywatched:"2019-05-20", rating:"9.5"},
+      {id:2, title:"Prison Break", favouriteCharacter:"Michael", recentlywatched:"2019-05-27", rating:"9.0"},
+      {id:3, title:"Breaking Bad", favouriteCharacter:"Jessie", recentlywatched:"2018-08-21", rating:"9.5"}
+    ];
+    return data;
+  }
+}
