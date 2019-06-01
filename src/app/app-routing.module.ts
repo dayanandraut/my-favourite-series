4d9a601b468d56e6b6b4a9e81456b2e2bf88ca19
dayanandraut <dayanandraut2015@.com>
2019-06-01 20:57:40 +0530

    configured routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ViewSeriesComponent } from './series/view-series/view-series.component';
+import { AddSeriesComponent } from './series/add-series/add-series.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"series", component:ViewSeriesComponent},
+  {path:"series/add", component:AddSeriesComponent},
+  {path:"", redirectTo:'/series', pathMatch:'full'}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
