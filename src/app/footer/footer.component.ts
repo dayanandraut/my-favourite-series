ba65f1ed6dac6e7b33c1650214624da2d705f7aa
dayanandraut <dayanandraut2015@.com>
2019-06-02 11:25:00 +0530

    designed footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
-
+  today:string;
   constructor() { }
 
   ngOnInit() {
+    let d = new Date();
+    let day = d.getDate();
+    let month = d.getMonth();
+    let year = d.getFullYear();    
+    this.today = year+"-"+month+"-"+day;
   }
 
 }
